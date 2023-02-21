885415aaa4ca3da6c57be1c2e23c4cfd242c2aa1
Kihwan Cho <ozlongblack@gmail.com>
2023-02-21 23:26:00 +1100

    build test

--- src/routes/api/useContentfulQuery.jsx
+++ src/routes/api/useContentfulQuery.jsx
@@ -2,12 +2,12 @@ import {useQuery} from '@shopify/hydrogen';
 
 export const useContentfulQuery = ({query, variables, key = []}) => {
   const {data} = useQuery(key, async () => {
-    const response = await fetch(import.meta.env.PRIVATE_CONTENTFUL_URL, {
+    const response = await fetch(import.meta.env.VITE_PRIVATE_CONTENTFUL_URL, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${
-          import.meta.env.PRIVATE_CONTENTFUL_ACCESS_TOKEN
+          import.meta.env.VITE_PRIVATE_CONTENTFUL_ACCESS_TOKEN
         }`,
       },
       body: JSON.stringify({
